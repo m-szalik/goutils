03ea1436ccf94f1b95f091c6012521c962abdebb
m-szalik <szalik@jsoftware.org>
2024-01-23 12:11:37 +0100

    PubSub test

---
+++ pubsub/pubsub_test.go
@@ -0,0 +1,60 @@
+package pubsub
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+type testSubscriber struct {
+	cancel context.CancelFunc
+	data   []int
+}
+
+func newTestSubscriber(parent context.Context, index int, ps PubSub[int]) *testSubscriber {
+	ctx, cancel := context.WithCancel(parent)
+	input := ps.NewSubscriber(ctx)
+	ts := &testSubscriber{
+		cancel: cancel,
+		data:   make([]int, 0),
+	}
+	go func() {
+		for {
+			select {
+			case d := <-input:
+				fmt.Printf("subscriber %d recived data: %d\n", index, d)
+				ts.data = append(ts.data, d)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return ts
+}
+
+func TestNewPubSubMultipleSubscribers(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.TODO(), 3*time.Minute) // fixme
+	defer cancel()
+	subscribers := make([]*testSubscriber, 0)
+	ps := NewPubSub[int](ctx)
+	subscribers = append(subscribers, newTestSubscriber(ctx, 0, ps))
+	subscribers = append(subscribers, newTestSubscriber(ctx, 1, ps))
+	publishCh := ps.NewPublisher()
+	publishCh <- 0
+	publishCh <- 1
+	delay()
+	subscribers[0].cancel()
+	delay()
+	publishCh <- 2
+	delay()
+	fmt.Println(subscribers[0].data)
+	fmt.Println(subscribers[1].data)
+	assert.Equal(t, []int{0, 1}, subscribers[0].data)
+	assert.Equal(t, []int{0, 1, 2}, subscribers[1].data)
+}
+
+func delay() {
+	time.Sleep(100 * time.Millisecond)
+}
