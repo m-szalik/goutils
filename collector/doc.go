133476d1dc749161e22169e9060a3acf4a738744
m-szalik <szalik@jsoftware.org>
2026-09-15 19:21:41 +0200

    Add package documentation

---
+++ collector/doc.go
@@ -0,0 +1,5 @@
+// Package collector provides simple generic collections: a rolling collection
+// that keeps the newest elements, a timed collection that drops old elements,
+// a growable collection, a set, a stack and a data points collector with
+// time window aggregations.
+package collector
