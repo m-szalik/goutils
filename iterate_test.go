ae6e325a46bd12881f4658403cda6735f88f885a
m-szalik <szalik@jsoftware.org>
2026-09-15 18:07:37 +0200

    IterateDeep: handle nil pointers, descend into interface values, format non-string map keys, deterministic map order

--- iterate_test.go
+++ iterate_test.go
@@ -36,6 +36,13 @@ func TestIterateDeep(t *testing.T) {
 		{startElement: []string{"a", "b", "c"}, expected: []iterationResult{{".[0]", "a"}, {".[1]", "b"}, {".[2]", "c"}}},
 		{startElement: &testStruct{[]string{"ala", "ma"}, 12, -11}, expected: []iterationResult{{".SArray[0]", "ala"}, {".SArray[1]", "ma"}, {".Num", 12}}},
 		{startElement: map[string]interface{}{"A": 1, "B": 1.2}, expected: []iterationResult{{".A", 1}, {".B", 1.2}}},
+		{startElement: struct {
+			P *int
+			V int
+		}{}, expected: []iterationResult{{".P", nil}, {".V", 0}}},
+		{startElement: map[string]any{"b": map[string]any{"c": []any{1, nil}}, "a": 2}, expected: []iterationResult{{".a", 2}, {".b.c[0]", 1}, {".b.c[1]", nil}}},
+		{startElement: map[int]string{7: "x"}, expected: []iterationResult{{".7", "x"}}},
+		{startElement: nil, expected: []iterationResult{{".", nil}}},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("test_%d_iterateDeep_over-%v", i, tt.startElement), func(t *testing.T) {
